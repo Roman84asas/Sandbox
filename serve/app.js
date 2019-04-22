f5c3a909298451d0f8a85ecb320afdabb5ca4fbb
RomanMereneanu <asasin84@mail.ru>
2019-04-22 15:50:22 +0300

    Mongoose

---
+++ serve/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+app.get("/", function(request, response){
+
+    response.send("<h1>Главная страница</h1>");
+});
+app.get("/about", function(request, response){
+
+    response.send("<h1>О сайте</h1>");
+});
+app.get("/contact", function(request, response){
+
+    response.send("<h1>Контакты</h1>");
+});
+app.listen(3000);
